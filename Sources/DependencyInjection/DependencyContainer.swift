9588cc6d8e2af84e3b21d9fffd6370268d2ea1d2
Kurski, Dzmitry | MCPD <ts-dzmitry.kurski@rakuten.com>
2023-02-01 11:46:20 +0900

    update: added dependency container

---
+++ Sources/DependencyInjection/DependencyContainer.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+final class DependencyContainer: DependencyProtocol {
+
+  static let shared = DependencyContainer()
+
+  private init() {}
+
+  var dependencies: [String: Any] = [:]
+
+  func register<Dependency>(type: Dependency.Type, dependency: Any) {
+      dependencies["\(type)"] = dependency
+  }
+
+  func resolve<Dependency>(type: Dependency.Type) -> Dependency? {
+    return dependencies["\(type)"] as? Dependency
+  }
+    
+}
