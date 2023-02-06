868339401c90fa8097c269a8083fd1837b2cf3e8
Kurski, Dzmitry | MCPD <ts-dzmitry.kurski@rakuten.com>
2023-02-06 14:09:30 +0900

    update: updated method documentation for CurrencyConverterProtocol

--- Sources/SimpleCurrencyConverter/CurrencyConverterProtocol.swift
+++ Sources/SimpleCurrencyConverter/CurrencyConverterProtocol.swift
@@ -10,8 +10,7 @@ public protocol CurrencyConverterProtocol {
     ///
     ///  - Parameter baseCurrency: A Currency object representing the base currency for which the exchange rate is desired.
     ///  - Parameter targetCurrencies: An array of Currency objects representing the target currencies for which the exchange rates are desired.
-    ///  - Parameter resolve: A closure that takes in a dictionary of exchange rates, where the keys are the currency codes of the target currencies and the values are the exchange rates (expressed as a Double)
-    ///  - Parameter reject: A closure that takes in an error object, it will be called if an error occurs while trying to get the exchange rates.
+    ///  - Parameter completion: An escaping closure that takes a Result<[Currency: Double], Error> as its argument. The Result value will contain a dictionary with the target currencies as keys and the corresponding exchange rates as values if the operation was successful, or an Error if the operation failed.
 
     func getExchangeRate(baseCurrency: Currency, targetCurrencies: [Currency], completion: @escaping (Result<[Currency: Double], Error>) -> Void)
     
@@ -22,20 +21,18 @@ public protocol CurrencyConverterProtocol {
     /// - Parameter amount: A Double value representing the amount of money to be converted.
     /// - Parameter baseCurrency: A Currency object representing the currency of the amount to be converted.
     /// - Parameter targetCurrency: A Currency object representing the currency to which the amount should be converted
-    /// - Parameter resolve: A closure that takes in a double value representing the converted amount, it will be called when the conversion is successful
-    /// - Parameter reject: A closure that takes in an error object, it will be called if an error occurs while trying to convert the amount
+    /// - Parameter completion: An escaping closure that takes a Result<Double, Error> as its argument. The Result value will contain the converted amount as a Double if the conversion was successful, or an Error if the conversion failed.
             
     func convert(amount: Double, baseCurrency: Currency, targetCurrency: Currency, completion: @escaping (Result<Double, Error>) -> Void)
     
-    ///     This function is used to convert an amount of money from one currency to multiple target currencies.
+    /// This function is used to convert an amount of money from one currency to multiple target currencies.
     ///
-    ///     This function uses the exchange rate between the base currency and the target currencies to convert the amount to all the target currencies, it returns the converted amount in all the target currencies through the resolve closure as a dictionary. If the request fails, the error is passed to the reject closure.
+    /// This function uses the exchange rate between the base currency and the target currencies to convert the amount to all the target currencies, it returns the converted amount in all the target currencies through the resolve closure as a dictionary. If the request fails, the error is passed to the reject closure.
     ///
-    ///        - Parameter amount: A Double value representing the amount of money to be converted.
-    ///        - Parameter baseCurrency: A Currency object representing the currency of the amount to be converted.
-    ///        - Parameter targetCurrency: An array of Currency objects representing the currencies to which the amount should be converted
-    ///        - Parameter resolve: A closure that takes in a dictionary of converted amount, where the keys are the currency codes of the target currencies and the values are the converted amount (expressed as a Double)
-    ///        - Parameter reject: A closure that takes in an error object, it will be called if an error occurs while trying to convert the amount
+    /// - Parameter amount: A Double value representing the amount of money to be converted.
+    /// - Parameter baseCurrency: A Currency object representing the currency of the amount to be converted.
+    /// - Parameter targetCurrency: An array of Currency objects representing the currencies to which the amount should be converted
+    /// - Parameter completion: An escaping closure that takes a Result<[Currency: Double], Error> as its argument. The Result value will contain a dictionary with the target currencies as keys and the corresponding exchange rates as values if the operation was successful, or an Error if the operation failed.
 
     func convert(amount: Double, baseCurrency: Currency, targetCurrencies: [Currency], completion: @escaping (Result<[Currency: Double], Error>) -> Void)
 }
